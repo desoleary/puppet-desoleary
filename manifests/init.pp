f57d8cc26d66b38bf6fffeeefca8ab273ddfd9dc
desoleary <desoleary@gmail.com>
2017-05-09 22:08:25 -0600

    - install apps I use

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,23 @@
 # This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+class desoleary {
+
+sudoers { 'installer':
+    users    => $::boxen_user,
+    hosts    => 'ALL',
+    commands => [
+      '(ALL) SETENV:NOPASSWD: /usr/sbin/installer',
+    ],
+    type     => 'user_spec',
+  }
+  
+  homebrew::tap { 'theseal/ssh-askpass': }
+  package { ['ssh-askpass']:
+    ensure => installed,
+    require => Homebrew::Tap['theseal/ssh-askpass']
+  }
+
+ package { ['tunnelblick','spectacle','the-unarchiver','rubymine','google-chrome', 'alfred', 'karabiner', 'moom', 'copyq', 'ccmenu', 'hipchat', 'balsamiq-mockups', 'datagrip', 'evernote', 'filezilla', 'gpgtools', 'iterm2', 'kodi', 'omnidisksweeper', 'pycharm', 'sequel-pro', 'sublime-text', 'virtualbox', 'webstorm', 'xscope']:
+    ensure   => installed,
+    provider => 'brewcask'
+  }
 }
